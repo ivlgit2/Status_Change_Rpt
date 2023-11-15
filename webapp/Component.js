2d1b5b969724cb4f3cc1903a173d146380b5dc9b
ivlgit2 <Ivlgit2@gmail.com>
2023-11-15 08:52:10 +0000

    Initial Commit : Aiswarya

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("bri.status_change_report.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
